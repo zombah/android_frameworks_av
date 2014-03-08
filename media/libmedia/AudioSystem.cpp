0eb2482bd626a4a0ce2fbcf5dbe1d65b161c97b2
zombah <zombah@gmail.com>
2014-03-08 19:26:08 +0400

    Nvidia OMX requirements wo condition patch from tonyp tree
    Signed-off-by: zombah <zombah@gmail.com>

--- media/libmedia/AudioSystem.cpp
+++ media/libmedia/AudioSystem.cpp
@@ -214,6 +214,11 @@ int AudioSystem::logToLinear(float volume)
     return volume ? 100 - int(dBConvertInverse * log(volume) + 0.5) : 0;
 }
 
+extern "C" status_t _ZN7android11AudioSystem21getOutputSamplingRateEPii(uint32_t* samplingRate, audio_stream_type_t streamType)
+{
+    return AudioSystem::getOutputFrameCount(samplingRate, streamType);
+}
+
 status_t AudioSystem::getOutputSamplingRate(uint32_t* samplingRate, audio_stream_type_t streamType)
 {
     audio_io_handle_t output;
@@ -254,6 +259,11 @@ status_t AudioSystem::getSamplingRate(audio_io_handle_t output,
             *samplingRate);
 
     return NO_ERROR;
+}
+
+extern "C" status_t _ZN7android11AudioSystem19getOutputFrameCountEPii(size_t* frameCount, audio_stream_type_t streamType)
+{
+    return AudioSystem::getOutputFrameCount(frameCount, streamType);
 }
 
 status_t AudioSystem::getOutputFrameCount(size_t* frameCount, audio_stream_type_t streamType)
